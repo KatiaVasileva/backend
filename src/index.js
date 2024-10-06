e8dea607d85eb10527b52b1f75cb4ce457b21e39
KatiaVasileva <eka.vasileva@gmail.com>
2024-10-06 17:58:07 +0500

    requests processed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-const http = require('http');
+const http = require("http");
+const getUsers = require("./modules/users");
 
 const server = http.createServer((request, response) => {
+  const ipAddress = "http://127.0.0.1";
+  const url = new URL(request.url, ipAddress);
+  const userName = url.searchParams.get("hello");
 
-    response.status = 200;
+  if (request.url === "/?hello=") {
+    response.statusCode = 400;
+    response.statusMessage = "Incorrect Data";
+    response.header = "Content-Type: text/plain";
+    response.write("Enter a name");
+    response.end();
+    return;
+  }
+
+  if (request.url === `/?hello=${userName}`) {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: text/plain";
+    response.write(`Hello, ${userName}!`);
+    response.end();
+    return;
+  }
+
+  if (request.url === "/?users") {
+    response.statusCode = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+    return;
+  }
+
+  if (request.url === "/") {
+    response.statusCode = 200;
     response.statusMessage = "OK";
     response.header = "Content-Type: text/plain";
     response.write("Hello, World!");
     response.end();
+    return;
+  }
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
-
+  response.statusCode = 500;
+  response.statusMessage = "Server Failed";
+  response.header = "Content-Type: text/plain";
+  response.write("");
+  response.end();
 });
 
 server.listen(3003, () => {
-    console.log("Сервер запущен по адресу http://127.0.0.1:3003");
-})
+  console.log("Сервер запущен по адресу http://127.0.0.1:3003");
+});
